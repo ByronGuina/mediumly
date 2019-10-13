85451b62934c1c9f9e40469e9695db927d85c8e0
ByronGuina <byron.guina@gmail.com>
2019-10-13 12:43:51 -0500

    Add Review documentation reasoning for using 3rd party service

--- packages/backend/src/api/utilities/getFeedAsJson.js
+++ packages/backend/src/api/utilities/getFeedAsJson.js
@@ -1,5 +1,9 @@
 import fetch from 'isomorphic-fetch';
 
+// We use a third-party tool to handle converting an RSS-feed to JSON
+// Some interesting conversation can come from this, e.g., what happens
+// if this service goes down? How much slower does this make the request
+// to our backend?
 const xmlToJsonApi = 'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/';
 
 export const getFeedAsJson = async (query) => {
