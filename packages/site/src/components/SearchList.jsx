b46a3ca8eb0f60dfb400126d7adb0c816c350145
ByronGuina <byron.guina@gmail.com>
2019-10-14 13:08:04 -0500

    Reverse entire component list instead of the data

--- packages/site/src/components/SearchList.jsx
+++ packages/site/src/components/SearchList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { reverse } from '../functional';
 
 export const SearchList = ({ searches }) => {
-    const Searches = reverse(searches).map((s, i) => <h1 key={`${s}-${i}`}>{s}</h1>);
+    const Searches = reverse(searches.map((s, i) => <h1 key={`${s}-${i}`}>{s}</h1>));
 
     return <>{Searches.length > 0 ? Searches : 'No previous searches.'}</>;
 };
--- packages/site/src/functional/index.js
+++ packages/site/src/functional/index.js
@@ -1,10 +1,8 @@
 export const takeLastFive = (arr) => {
-    // console.log('taking last 5', arr);
     return arr.slice(-5);
 };
 
 export const reverse = (arr) => {
-    // console.log('reversing', arr);
     return arr.reverse();
 };
 
